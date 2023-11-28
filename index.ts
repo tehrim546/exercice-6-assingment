173715869277d2896cc13863526e81336c1d3eae
= <=>
2023-11-28 10:51:28 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+//Exercise 6:  Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
+const result: number = ((10 + 5) * 3 - 2 / (4 % 3 ) -7);
+console.log("Result:", result);
